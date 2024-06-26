0b0ba1cf54df62a8e8ef48eb418082414e0e5c04
MarekCZFM <89865416+MarekCZFM@users.noreply.github.com>
2024-06-26 22:07:35 +0200

    Zalozeni collision systemu a pridani akcelerace a rychlosti

--- ball.py
+++ ball.py
@@ -1,11 +1,23 @@
+import math
 import pygame
 
 class Ball:
-    def __init__(self, x, y, radius, color):
+    def __init__(self, x, y, radius, color, width, velocity, acceleration):
         self.x = x
         self.y = y
         self.radius = radius
         self.color = color  # Example color, red
+        self.width = width
+        self.acceleration = acceleration
+        self.velocity = velocity
 
     def draw(self, screen):
-        pygame.draw.circle(screen, self.color, (self.x, self.y), self.radius)
+        pygame.draw.circle(screen, self.color, (self.x, self.y), self.radius, self.width)
+
+    def check_for_collisions_with_circle(self, secondBall):
+        biggerBall = max([self, secondBall], key=radius)
+        smallerBall = min([self, secondBall], key=radius)
+        x = max(self.x, secondBall.x) - min(self.x, secondBall.x)
+        y = max(self.y, secondBall.y) - min(self.y, secondBall.y)
+        distance = math.sqrt(x ** 2 + y ** 2)
+        return biggerBall.radius - biggerBall.width // 2 - smallerBall.radius - smallerBall.width // 2 <= 0
--- main.py
+++ main.py
@@ -5,8 +5,9 @@ from ball import Ball
 
 SIRKA, VYSKA = 1080//2, 1920//2           # Full HD bylo pro me moc :D
 obrazovka = pygame.display.set_mode((SIRKA, VYSKA))
+pygame.display.set_caption("My balls")
 
-kruznice = Ball(SIRKA//2, VYSKA//2, 50)
+kruznice = Ball(SIRKA//2, VYSKA//2, 250, "#FFFFFF", 3, (0, 3))
 clock = Clock()
 running = True
 while running:
