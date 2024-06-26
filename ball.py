2fcbbc129c81f5a67cf6a5e73a24544d78f66fd9
MarekCZFM <89865416+MarekCZFM@users.noreply.github.com>
2024-06-26 20:42:50 +0200

    my balls

--- ball.py
+++ ball.py
@@ -1,11 +1,11 @@
 import pygame
 
 class Ball:
-    def __init__(self, x, y, radius):
+    def __init__(self, x, y, radius, color):
         self.x = x
         self.y = y
         self.radius = radius
-        self.color = (255, 0, 0)  # Example color, red
+        self.color = color  # Example color, red
 
     def draw(self, screen):
         pygame.draw.circle(screen, self.color, (self.x, self.y), self.radius)
